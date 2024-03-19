fedbb4866f42a2cb30a087403b7ae67231c7c9ea
meenakshi1237 <meenakshinaidua@gmail.com>
2024-03-19 14:20:14 +0530

    all entities created

--- src/main/java/com/app/party/genine/dao/BookingDao.java
+++ src/main/java/com/app/party/genine/dao/BookingDao.java
@@ -1,9 +1,11 @@
 package com.app.party.genine.dao;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import com.app.party.genine.repository.BookingRepository;
 
+@Repository
 public class BookingDao {
 	@Autowired
 	private BookingRepository bookingRepository;
--- src/main/java/com/app/party/genine/service/BookingService.java
+++ src/main/java/com/app/party/genine/service/BookingService.java
@@ -1,9 +1,10 @@
 package com.app.party.genine.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.app.party.genine.dao.BookingDao;
-
+@Service
 public class BookingService {
 	@Autowired
 	private BookingDao bookingDao;
