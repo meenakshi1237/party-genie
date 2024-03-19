537691462f1d06f3c13ea383bfdfba48e2998d65
thejuthunga <thejasthejashree@gmail.com>
2024-03-19 14:27:04 +0530

    pulling from dev

--- src/main/java/com/app/party/genine/controller/CustomerController.java
+++ src/main/java/com/app/party/genine/controller/CustomerController.java
@@ -1,17 +1,26 @@
 package com.app.party.genine.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.app.party.genine.dto.CustomerRequest;
+import com.app.party.genine.dto.CustomerResponse;
+import com.app.party.genine.dto.ResponseStructure;
 import com.app.party.genine.service.CustomerService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/customer")
 public class CustomerController {
-	
+
 	@Autowired
 	private CustomerService customerService;
-	
-	
+
+	public ResponseEntity<ResponseStructure<CustomerResponse>> customerRegister(@Valid @RequestBody CustomerRequest customerRequest){
+		return null;
+	}
 }
--- src/main/java/com/app/party/genine/dao/BookingDao.java
+++ src/main/java/com/app/party/genine/dao/BookingDao.java
@@ -1,6 +1,7 @@
 package com.app.party.genine.dao;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import com.app.party.genine.repository.BookingRepository;
 
