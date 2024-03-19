8d65172e430296ba9e2f569346440f64d8eacff1
meenakshi1237 <meenakshinaidua@gmail.com>
2024-03-19 11:38:40 +0530

    booking entity created

---
+++ src/main/java/com/app/party/genine/service/BookingService.java
@@ -0,0 +1,10 @@
+package com.app.party.genine.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.app.party.genine.dao.BookingDao;
+
+public class BookingService {
+	@Autowired
+	private BookingDao bookingDao;
+}
